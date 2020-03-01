020ddcb4ccc98a6005c391b1e3612377fe01e377
Jay <bluehoped@hanmail.net>
2020-03-01 20:16:03 +0900

    수정 삭제 추가

--- springboot-webservice/src/main/java/com/jay/clone/springboot/service/posts/PostsService.java
+++ springboot-webservice/src/main/java/com/jay/clone/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jay.clone.springboot.service.posts;
 
 import com.jay.clone.springboot.domain.posts.Posts;
 import com.jay.clone.springboot.domain.posts.PostsRepository;
+import com.jay.clone.springboot.web.dto.PostsListResponseDto;
 import com.jay.clone.springboot.web.dto.PostsResponseDto;
 import com.jay.clone.springboot.web.dto.PostsSaveRequestDto;
 import com.jay.clone.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,8 +36,21 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
